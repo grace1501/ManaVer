9494fc75727f0301d6ecddbf9818d9996416623c
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-04-14 17:19:51 +0000

    created folders resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  resources :folders
   devise_for :users
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
   
 end
