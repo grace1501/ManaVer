ddfe8b0edc08e69ab716376344890583b2506ad0
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-04-15 04:17:09 +0000

    set up aws for file storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,10 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+
+  # set Active Storage to use S3 in production
+  config.active_storage.service = :amazon
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Don't care if the mailer can't send.
