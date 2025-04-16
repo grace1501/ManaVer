9cea962edae41dc8e3d5b8b1103e1d50eb90fd6a
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-04-16 16:18:12 +0000

    implement separate routes for signed-in users and guests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  root "folders#index"
   devise_for :users
+
+  unauthenticated do
+    root "pages#about"
+  end
   
+  authenticated :user do
+    root "folders#index", as: :authenticated_root
+  end
+
   resources :folders
   resources :versions, except: [:index]  
   
+  get "about", { :controller => "pages", :action => "about"}
 end
